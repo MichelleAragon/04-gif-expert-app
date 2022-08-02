cf5918422a911853ec30752f25f5a1cf176f0c3e
MichelleAragon <michellearagon11@gmail.com>
2022-08-02 12:18:19 -0300

    Updated

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,6 +1,6 @@
 export const getGif = async (category) => {
         
-    const url =`https://api.giphy.com/v1/gifs/search?api_key=NwBRCDbeDN7KZL8JzVBE1cnolpU3Unav&q=${category}&limit=20`;
+    const url =`https://api.giphy.com/v1/gifs/search?api_key=NwBRCDbeDN7KZL8JzVBE1cnolpU3Unav&q=${category}&limit=10`;
 
     const respuesta = await fetch(url); 
 
