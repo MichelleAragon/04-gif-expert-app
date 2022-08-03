73f88aa515beac3240f233e97e49a4a5ec016362
MichelleAragon <michellearagon11@gmail.com>
2022-08-03 16:22:55 -0300

    fix image alt proprty title

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -7,7 +7,7 @@ export const GifGridItem = ({title, url }) => {
 
     return (
         <div className="card">
-            <img src={url} alt= {{title}}/>
+            <img src={url} alt= {title}/>
             <p>{ title }</p>
         </div>
 
